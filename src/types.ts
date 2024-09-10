2eab563532ffdc57af83d0ccc3cbaa3493282d7a
Romaric Zounlome <rzounlom@gmail.com>
2024-09-09 23:23:38 -0400

    add user data

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+// src/data/usersData.ts
+export interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  avatar: string;
+  title: string;
+  yos: number; // Years of Service
+  active: boolean;
+}
